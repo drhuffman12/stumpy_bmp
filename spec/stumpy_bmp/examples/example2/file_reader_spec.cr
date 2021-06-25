15fd637f98de083cdead0d8d1f0859c25a432fc6
Daniel Huffman <drhuffman12@gmail.com>
2021-06-25 00:10:13 -0400

    drhuffman12_update_stumpy_bmp_part_3 adjust tests for 'example1' and 'example2'

--- spec/stumpy_bmp/examples/example2/file_reader_spec.cr
+++ spec/stumpy_bmp/examples/example2/file_reader_spec.cr
@@ -1,27 +1,31 @@
 require "./../../../spec_helper"
 
-Spectator.describe StumpyBMP::FileData do
+Spectator.describe StumpyBMP::FileReader do
   context "when file_path given (and it is a valid bmp file)" do
     # when given a valid bmp file...
-    let(file_path) { "spec/stumpy_bmp/examples/example1/image.bmp" }
+    let(file_path) { "spec/stumpy_bmp/examples/example2/image.bmp" }
     let(file_data) { StumpyBMP::FileData.new(file_path) }
+    let(file_reader) { StumpyBMP::FileReader.new(file_data) }
 
     # we expect...
-    let(file_size_expected) { 322 }
+    let(file_size_expected) { 222 }
     let(file_bytes_expected) {
       [
-        66, 77, 66, 1, 0,
+        66, 77, 222, 0, 0,
         0, 0, 0, 0, 0,
-        122, 0, 0, 0, 108,
-        0, 0, 0, 6, 0,
-        0, 0, 10, 0, 0,
-        0, 1, 0, 24, 0,
-        0, 0, 0, 0, 200,
+        138, 0, 0, 0, 124,
+        0, 0, 0, 3, 0,
+        0, 0, 7, 0, 0,
+        0, 1, 0, 32, 0,
+        3, 0, 0, 0, 84,
         0, 0, 0, 19, 11,
         0, 0, 19, 11, 0,
         0, 0, 0, 0, 0,
-        0, 0, 0, 0, 66,
-        71, 82, 115, 0, 0,
+        0, 0, 0, 0, 0,
+        0, 255, 0, 0, 255,
+        0, 0, 255, 0, 0,
+        0, 0, 0, 0, 255,
+        66, 71, 82, 115, 0,
         0, 0, 0, 0, 0,
         0, 0, 0, 0, 0,
         0, 0, 0, 0, 0,
@@ -31,50 +35,27 @@ Spectator.describe StumpyBMP::FileData do
         0, 0, 0, 0, 0,
         0, 0, 0, 0, 0,
         0, 0, 0, 0, 0,
-        0, 2, 0, 0, 0,
+        0, 0, 2, 0, 0,
         0, 0, 0, 0, 0,
         0, 0, 0, 0, 0,
-        0, 0, 255, 255, 255,
-        255, 255, 255, 255, 255,
-        255, 0, 0, 0, 1,
-        0, 48, 0, 0, 48,
-        0, 0, 255, 255, 255,
-        1, 0, 48, 1, 0,
-        48, 4, 33, 112, 0,
-        48, 144, 0, 96, 146,
-        0, 0, 255, 255, 255,
-        2, 0, 48, 255, 255,
-        255, 1, 0, 48, 2,
-        0, 48, 48, 24, 162,
-        0, 0, 0, 0, 48,
-        1, 0, 64, 76, 73,
-        112, 0, 0, 0, 255,
-        255, 255, 0, 0, 156,
-        0, 0, 1, 0, 48,
-        8, 2, 162, 2, 0,
-        48, 2, 0, 48, 255,
-        255, 255, 0, 16, 156,
-        0, 0, 1, 0, 48,
-        0, 16, 144, 2, 0,
-        48, 2, 0, 48, 255,
-        255, 255, 0, 0, 48,
-        0, 0, 2, 0, 48,
-        24, 24, 168, 3, 0,
-        48, 12, 137, 112, 3,
-        0, 48, 3, 0, 48,
-        0, 0, 1, 0, 48,
-        0, 2, 164, 2, 0,
-        48, 0, 0, 48, 24,
-        2, 166, 255, 255, 255,
-        0, 0, 2, 0, 48,
-        0, 64, 156, 0, 0,
-        28, 1, 0, 48, 160,
-        153, 112, 255, 255, 255,
-        0, 0, 255, 255, 255,
-        255, 255, 255, 0, 0,
-        0, 0, 80, 146, 3,
-        0, 48, 255, 255, 255,
-        0, 0,
+        0, 0, 0, 0, 0,
+        255, 255, 51, 0, 51,
+        255, 255, 0, 0, 255,
+        0, 51, 51, 255, 0,
+        0, 119, 255, 119, 0,
+        0, 255, 0, 255, 0,
+        255, 0, 119, 0, 255,
+        0, 0, 0, 255, 51,
+        51, 51, 123, 119, 119,
+        119, 123, 187, 187, 187,
+        11, 0, 255, 0, 255,
+        119, 255, 119, 255, 255,
+        255, 255, 255, 0, 119,
+        119, 255, 119, 119, 255,
+        255, 255, 119, 119, 255,
+        0, 0, 255, 255, 119,
+        0, 119, 255, 255, 0,
+        0, 255,
       ]
     }
 
@@ -82,21 +63,21 @@ Spectator.describe StumpyBMP::FileData do
     let(file_ident_header_ords_expected) { [66, 77] }
     let(rs1_expected) { 0 }
     let(rs2_expected) { 0 }
-    let(offset_expected) { 122 }
-    let(header_size_expected) { 108 }
-    let(width_expected) { 6 }
-    let(height_expected) { 10 }
+    let(offset_expected) { 138 }
+    let(header_size_expected) { 124 }
+    let(width_expected) { 3 }
+    let(height_expected) { 7 }
     let(color_planes_expected) { 1 }
-    let(bits_expected) { 24 }
-    let(compression_expected) { 0 }
-    let(image_size_expected) { 200 }
+    let(bits_expected) { 32 }
+    let(compression_expected) { 3 }
+    let(image_size_expected) { 84 }
     let(res_x_expected) { 2835 }
     let(res_y_expected) { 2835 }
     let(color_numbers_expected) { 0 }
 
     describe "#initialize" do
       it "does not raise" do
-        expect { StumpyBMP::FileData.new(file_path) }.not_to raise_error
+        expect { StumpyBMP::FileReader.new(file_data) }.not_to raise_error
       end
 
       it "is valid" do
@@ -106,34 +87,34 @@ Spectator.describe StumpyBMP::FileData do
 
       context "sets expected values for instance variable" do
         it "file_path" do
-          expect(file_data.file_path.empty?).to be_false
+          expect(file_reader.file_data.file_path.empty?).to be_false
         end
 
         it "file_bytes" do
           # because at initialization, we have not *yet* read the file data
-          expect(file_data.file_bytes.empty?).to be_true
+          expect(file_reader.file_data.file_bytes.empty?).to be_true
         end
       end
 
       context "errors do NOT include" do
         it "file_path" do
-          expect(file_data.errors.keys.to_a).not_to contain(:file_path)
+          expect(file_reader.file_data.errors.keys.to_a).not_to contain(:file_path)
         end
       end
 
       context "errors do include" do
         # because at initialization, we have not *yet* read the file data
         it "file_ident_header_ords" do
-          expect(file_data.errors.keys.to_a).to contain(:file_ident_header_ords)
-          expect(file_data.errors[:file_ident_header_ords]).to eq("Not a BMP file")
+          expect(file_reader.file_data.errors.keys.to_a).to contain(:file_ident_header_ords)
+          expect(file_reader.file_data.errors[:file_ident_header_ords]).to eq("Not a BMP file")
         end
       end
     end
 
-    describe "#read_data" do
+    describe "#read" do
       before_each do
-        allow(file_data).to receive(:read_bytes).and_return(nil)
-        allow(file_data).to receive(:read_header_data).and_return(nil)
+        allow(file_reader).to receive(:read_bytes).and_return(nil)
+        allow(file_reader).to receive(:read_header_data).and_return(nil)
       end
 
       context "calls" do
@@ -141,16 +122,16 @@ Spectator.describe StumpyBMP::FileData do
           # allow(file_data).to receive(:read_bytes).and_return(nil)
           # allow(file_data).to receive(:read_header_data).and_return(nil)
 
-          expect(file_data).to receive(:read_bytes).and_return(nil)
-          file_data.read_data
+          expect(file_reader).to receive(:read_bytes).and_return(nil)
+          file_reader.read
         end
 
         pending "read_header_data" do
           # allow(file_data).to receive(:read_bytes).and_return(nil)
           # allow(file_data).to receive(:read_header_data).and_return(nil)
 
-          expect(file_data).to receive(:read_header_data)
-          file_data.read_data
+          expect(file_reader).to receive(:read_header_data)
+          file_reader.read
         end
       end
     end
@@ -158,36 +139,36 @@ Spectator.describe StumpyBMP::FileData do
     describe "#read_bytes" do
       context "does call" do
         pending "File.open" do
-          expect(file_data.file_path.empty?).to be_false
+          expect(file_reader.file_data.file_path.empty?).to be_false
           expect(File).to receive(:open).with(file_path)
-          file_data.read_bytes
+          file_reader.read_bytes
         end
       end
 
       it "sets @file_bytes with expected (mock) data" do
-        expect(file_data.file_bytes).to eq(Array(UInt8).new)
-        file_data.read_bytes
-        expect(file_data.file_bytes).to eq(file_bytes_expected)
+        expect(file_reader.file_data.file_bytes).to eq(Array(UInt8).new)
+        file_reader.read_bytes
+        expect(file_reader.file_data.file_bytes).to eq(file_bytes_expected)
       end
 
       it "returns expected (mock) data" do
-        expect(file_data.read_bytes).to eq(file_bytes_expected)
+        expect(file_reader.read_bytes).to eq(file_bytes_expected)
       end
     end
 
     describe "#read_header_data" do
       before_each do
-        file_data.read_bytes
+        file_reader.read_bytes
       end
 
       context "when file_path IS given (and not empty and is for a valid bmp file)" do
         before_each do
-          file_data.read_header_data
+          file_reader.read_header_data
         end
 
         context "does set expected value for instance variable" do
           context "@file_ident_header_ords re" do
-            let(variable_set) { file_data.file_ident_header_ords }
+            let(variable_set) { file_reader.file_data.file_ident_header_ords }
             let(value_expected) { file_ident_header_ords_expected }
 
             it "class" do
@@ -200,7 +181,7 @@ Spectator.describe StumpyBMP::FileData do
           end
 
           context "@file_size re" do
-            let(variable_set) { file_data.file_size }
+            let(variable_set) { file_reader.file_data.file_size }
             let(value_expected) { file_size_expected }
 
             it "class" do
@@ -213,7 +194,7 @@ Spectator.describe StumpyBMP::FileData do
           end
 
           context "@rs1 re" do
-            let(variable_set) { file_data.rs1 }
+            let(variable_set) { file_reader.file_data.rs1 }
             let(value_expected) { rs1_expected }
             it "class" do
               expect(variable_set.class).to eq(UInt32)
@@ -225,7 +206,7 @@ Spectator.describe StumpyBMP::FileData do
           end
 
           context "@rs2 re" do
-            let(variable_set) { file_data.rs2 }
+            let(variable_set) { file_reader.file_data.rs2 }
             let(value_expected) { rs2_expected }
             it "class" do
               expect(variable_set.class).to eq(UInt32)
@@ -237,10 +218,10 @@ Spectator.describe StumpyBMP::FileData do
           end
 
           context "@offset re" do
-            let(variable_set) { file_data.offset }
+            let(variable_set) { file_reader.file_data.offset }
             let(value_expected) { offset_expected }
             it "class" do
-              expect(file_data.offset.class).to eq(UInt32)
+              expect(file_reader.file_data.offset.class).to eq(UInt32)
             end
 
             it "value" do
@@ -249,7 +230,7 @@ Spectator.describe StumpyBMP::FileData do
           end
 
           context "@header_size re" do
-            let(variable_set) { file_data.header_size }
+            let(variable_set) { file_reader.file_data.header_size }
             let(value_expected) { header_size_expected }
             it "class" do
               expect(variable_set.class).to eq(UInt32)
@@ -261,7 +242,7 @@ Spectator.describe StumpyBMP::FileData do
           end
 
           context "@width re" do
-            let(variable_set) { file_data.width }
+            let(variable_set) { file_reader.file_data.width }
             let(value_expected) { width_expected }
             it "class" do
               expect(variable_set.class).to eq(UInt32)
@@ -273,7 +254,7 @@ Spectator.describe StumpyBMP::FileData do
           end
 
           context "@height re" do
-            let(variable_set) { file_data.height }
+            let(variable_set) { file_reader.file_data.height }
             let(value_expected) { height_expected }
             it "class" do
               expect(variable_set.class).to eq(UInt32)
@@ -285,7 +266,7 @@ Spectator.describe StumpyBMP::FileData do
           end
 
           context "@color_planes re" do
-            let(variable_set) { file_data.color_planes }
+            let(variable_set) { file_reader.file_data.color_planes }
             let(value_expected) { color_planes_expected }
             it "class" do
               expect(variable_set.class).to eq(UInt32)
@@ -297,7 +278,7 @@ Spectator.describe StumpyBMP::FileData do
           end
 
           context "@bits re" do
-            let(variable_set) { file_data.bits }
+            let(variable_set) { file_reader.file_data.bits }
             let(value_expected) { bits_expected }
             it "class" do
               expect(variable_set.class).to eq(UInt32)
@@ -309,7 +290,7 @@ Spectator.describe StumpyBMP::FileData do
           end
 
           context "@compression re" do
-            let(variable_set) { file_data.compression }
+            let(variable_set) { file_reader.file_data.compression }
             let(value_expected) { compression_expected }
             it "class" do
               expect(variable_set.class).to eq(UInt32)
@@ -321,7 +302,7 @@ Spectator.describe StumpyBMP::FileData do
           end
 
           context "@image_size re" do
-            let(variable_set) { file_data.image_size }
+            let(variable_set) { file_reader.file_data.image_size }
             let(value_expected) { image_size_expected }
             it "class" do
               expect(variable_set.class).to eq(UInt32)
@@ -333,7 +314,7 @@ Spectator.describe StumpyBMP::FileData do
           end
 
           context "@res_x re" do
-            let(variable_set) { file_data.res_x }
+            let(variable_set) { file_reader.file_data.res_x }
             let(value_expected) { res_x_expected }
             it "class" do
               expect(variable_set.class).to eq(UInt32)
@@ -345,7 +326,7 @@ Spectator.describe StumpyBMP::FileData do
           end
 
           context "@res_y re" do
-            let(variable_set) { file_data.res_y }
+            let(variable_set) { file_reader.file_data.res_y }
             let(value_expected) { res_y_expected }
             it "class" do
               expect(variable_set.class).to eq(UInt32)
@@ -357,7 +338,7 @@ Spectator.describe StumpyBMP::FileData do
           end
 
           context "@color_numbers re" do
-            let(variable_set) { file_data.color_numbers }
+            let(variable_set) { file_reader.file_data.color_numbers }
             let(value_expected) { color_numbers_expected }
             it "class" do
               expect(variable_set.class).to eq(UInt32)
@@ -371,37 +352,37 @@ Spectator.describe StumpyBMP::FileData do
       end
     end
 
-    describe "#write_data" do
-      let(temp_file_name) { "test_image.bmp" }
-      let(temp_file_path) { File.tempname(temp_file_name) }
-      let(temp_file_data) { StumpyBMP::FileData.new(temp_file_path).tap { |fd| fd.read_data } }
+    # describe "#write_data" do
+    #   let(temp_file_name) { "test_image.bmp" }
+    #   let(temp_file_path) { File.tempname(temp_file_name) }
+    #   let(temp_file_data) { StumpyBMP::FileData.new(temp_file_path).tap { |fd| fd.read_data } }
 
-      before_each do
-        file_data.read_data
-        p! temp_file_path
-      end
+    #   before_each do
+    #     file_data.read_data
+    #     p! temp_file_path
+    #   end
 
-      after_each do
-        begin
-          # TODO: Find out 'File.delete' of a temp file fails on Windows w/ "Permission denied"
-          File.delete(temp_file_path) if File.exists?(temp_file_path)
-        rescue ex
-          p! ex
-        end
-      end
+    #   after_each do
+    #     begin
+    #       # TODO: Find out 'File.delete' of a temp file fails on Windows w/ "Permission denied"
+    #       File.delete(temp_file_path) if File.exists?(temp_file_path)
+    #     rescue ex
+    #       p! ex
+    #     end
+    #   end
 
-      it "writes the expected number of bytes" do
-        file_size_written = file_data.write_data(temp_file_path)
-        expect(file_size_written).to eq(file_size_expected)
-      end
+    #   it "writes the expected number of bytes" do
+    #     file_size_written = file_data.write_data(temp_file_path)
+    #     expect(file_size_written).to eq(file_size_expected)
+    #   end
 
-      it "written file bytes match the original file bytes" do
-        file_data.write_data(temp_file_path)
+    #   it "written file bytes match the original file bytes" do
+    #     file_data.write_data(temp_file_path)
 
-        # NOTE: 'Each row in the Pixel array is padded to a multiple of 4 bytes in size'
-        #   So, 'file.size' and 'file_data.file_bytes.size might vary'.
-        expect(temp_file_data.file_bytes).to eq(file_data.file_bytes)
-      end
-    end
+    #     # NOTE: 'Each row in the Pixel array is padded to a multiple of 4 bytes in size'
+    #     #   So, 'file.size' and 'file_data.file_bytes.size might vary'.
+    #     expect(temp_file_data.file_bytes).to eq(file_data.file_bytes)
+    #   end
+    # end
   end
 end
